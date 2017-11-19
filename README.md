33e1ef9415db5de97d3a9b980993ce555c3c3532
Andrew Batallones <batallonesa@gmail.com>
2017-11-18 21:27:38 -0800

    changing to materialize css

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Andrew Batallones
 
 This is my profile site, where you can see all of my projects and other things that I have done. As of right now, I do not have a lot of content, but I am hoping that as I learn more and more each day, that I can have more projects under my belt.
+
+*Run SASS
+```
+$ sass --watch css/app.scss:css/app.css
+```
--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,4 @@
 $(function () {
-  $('#about').collapse('show');
-
-  $('#about').on('show.bs.collapse', function () {
-    $('#projects').collapse('hide');
-    $('#contact').collapse('hide');
-  });
-
-  $('#projects').on('show.bs.collapse', function () {
-    $('#about').collapse('hide');
-    $('#contact').collapse('hide');
-  });
-
-  $('#contact').on('show.bs.collapse', function () {
-    $('#about').collapse('hide');
-    $('#projects').collapse('hide');
-  });
+  $('.collapsible').collapsible();
+  $('.carousel').carousel();
 });
